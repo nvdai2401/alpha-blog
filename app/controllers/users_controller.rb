35c326c5c6fe5b6dc7d6db7451888083dad659d5
nvdai2401 <nvdai2401@gmail.com>
2020-11-13 14:14:47 +0700

    develop update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_params
+
   def new
     @user = User.new
   end
@@ -14,9 +16,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else 
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_params
+    @user = User.find(params[:id])
   end
 end
