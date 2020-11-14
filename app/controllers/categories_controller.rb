2cc2206d6ea55eceb34780c2351c055604bc36f6
nvdai2401 <nvdai2401@gmail.com>
2020-11-14 07:35:52 +0700

    complete categories create action and functional test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,5 @@
 class CategoriesController < ApplicationController
 
-  def new
-
-  end
-
   def index
 
   end
@@ -11,4 +7,26 @@ class CategoriesController < ApplicationController
   def show
 
   end
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      respond_to @category
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end 
